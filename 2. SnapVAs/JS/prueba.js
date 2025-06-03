94ec025ce15dc8b7bc3569a621901e188ec36b7e
DFPARRAT <35248688+dfparrat@users.noreply.github.com>
2025-06-03 11:20:50 -0500

    Delete 2. SnapVAs directory

--- 2. SnapVAs/JS/prueba.js
+++
@@ -1,6 +0,0 @@
-function mostrarMensaje()
-{
-    var formwapp = document.querySelector('#formwapp');
-    //var inputName = document.querySelector('#nombre').value
-    alert("Nombre: " + document.querySelector('#nombre').value)
-}
